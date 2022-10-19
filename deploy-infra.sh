9752c615a0697d3cfde910199f4211e5314fa80b
Levan Apkhazava <levan.apkhazava@scandiweb.com>
2022-10-20 02:48:20 +0400

    ;)

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -10,6 +10,13 @@ AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
   --query "Account" --output text`
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID" 
 
+# Generate a personal access token with repo and admin:repo_hook
+#    permissions from https://github.com/settings/tokens
+GH_ACCESS_TOKEN=$(cat ~/.github/jeffrep-access-token)
+GH_OWNER=$(cat ~/.github/jeffrep-owner)
+GH_REPO=$(cat ~/.github/jeffrep-repo)
+GH_BRANCH=master
+
 # Deploy the CloudFormation template
 echo -e "\n\n=========== Deploying main.yml ==========="
 aws cloudformation deploy \
@@ -20,8 +27,12 @@ aws cloudformation deploy \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
-    CodePipelineBucket=$CODEPIPELINE_BUCKET \
-    CloudFormationBucket=$CFN_BUCKET
+    EC2InstanceType=$EC2_INSTANCE_TYPE \
+    GitHubOwner=$GH_OWNER \
+    GitHubRepo=$GH_REPO \
+    GitHubBranch=$GH_BRANCH \
+    GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
 
     # If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
