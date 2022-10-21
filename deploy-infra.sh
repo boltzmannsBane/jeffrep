29906591a3cec556b6c7baa10617c862c7c41b93
Levan Apkhazava <levan.apkhazava@scandiweb.com>
2022-10-21 17:45:53 +0400

    modified setup files

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -3,22 +3,31 @@
 STACK_NAME=jeffrep
 REGION=us-east-1
 CLI_PROFILE=jeffrep
-
 EC2_INSTANCE_TYPE=t2.micro
 
-AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
-  --query "Account" --output text`
-CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID" 
-
-# Generate a personal access token with repo and admin:repo_hook
-#    permissions from https://github.com/settings/tokens
 GH_ACCESS_TOKEN=$(cat ~/.github/jeffrep-access-token)
 GH_OWNER=$(cat ~/.github/jeffrep-owner)
 GH_REPO=$(cat ~/.github/jeffrep-repo)
 GH_BRANCH=master
 
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile jeffrep --query "Account" --output text`
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+
+echo $CODEPIPELINE_BUCKET
+
+# Deploys static resources
+echo "\n\n=========== Deploying setup.yml ==========="
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME-setup \
+  --template-file setup.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides CodePipelineBucket=$CODEPIPELINE_BUCKET
+
 # Deploy the CloudFormation template
-echo -e "\n\n=========== Deploying main.yml ==========="
+echo "\n\n=========== Deploying main.yml ==========="
 aws cloudformation deploy \
   --region $REGION \
   --profile $CLI_PROFILE \
@@ -26,8 +35,7 @@ aws cloudformation deploy \
   --template-file main.yml \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
-  --parameter-overrides \
-    EC2InstanceType=$EC2_INSTANCE_TYPE \
+  --parameter-overrides EC2InstanceType=$EC2_INSTANCE_TYPE \
     GitHubOwner=$GH_OWNER \
     GitHubRepo=$GH_REPO \
     GitHubBranch=$GH_BRANCH \
@@ -37,6 +45,6 @@ aws cloudformation deploy \
     # If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
   aws cloudformation list-exports \
-    --profile awsbootstrap \
+    --profile jeffrep \
     --query "Exports[?Name=='InstanceEndpoint'].Value"
 fi
