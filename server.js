d3e5fe7b7fd13031c4d7324ec82d814d776ce2d2
Levan Apkhazava <levan.apkhazava@scandiweb.com>
2022-10-21 19:46:48 +0400

    automatic deployment test

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const { hostname } = require('os');
 const http = require('http');
-const message = 'Hello World\n'; 
-const port = 8080; 
-const server = http.createServer((req, res) => { 
+const message = 'Hello Cloud\n';
+const port = 8080;
+const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end(message);
