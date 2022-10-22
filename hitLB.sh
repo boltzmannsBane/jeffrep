9f0f9e0b89eb4447bd100be3127de11f443fbf29
Levan Apkhazava <levan.apkhazava@scandiweb.com>
2022-10-22 22:54:57 +0400

    Added a production stack

--- hitLB.sh
+++ hitLB.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for run in {1..10}
-do curl -s http://jeffr-LoadB-19VUU5L1KBVAK-1632801248.us-east-1.elb.amazonaws.com:80
+do curl -s http://jeffr-LoadB-CN04457C4UWA-1574504987.us-east-1.elb.amazonaws.com:80
 done | sort | uniq -c
